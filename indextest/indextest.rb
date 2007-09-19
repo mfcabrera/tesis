e87f5b3c770121a3127640d6656282165f8195f6
mfcabrera <mfcabrera@e803852c-e837-0410-abc8-f54d6c4cb6c2>
2007-09-19 21:13:45 +0000

    mas cambios
    git-svn-id: http://xue.unalmed.edu.co/svn/tesis@23 e803852c-e837-0410-abc8-f54d6c4cb6c2

--- indextest/indextest.rb
+++ indextest/indextest.rb
@@ -7,73 +7,6 @@ require 'rubygems'
 require 'ferret'
 include Ferret
 
-
-#Ferret extension
-
-class Index::IndexReader 
-  def tf(docId)
-    #te = index.reader.terms(:text)
-    tv = self.term_vector(docId,:text)
-    vect = Hash.new
-    tv.terms.each {
-        |t| puts "index::tf:  #{t.text}  with  #{t.positions.size}" 
-        vect[t.text] = t.positions.size
-    }
-    vect
-  end
-  
-  def sum_nk      
-  te = self.terms(:text)
-  sum_nk = 0
-    te.each {|term, doc_freq| 
-		puts "#{term} occured #{doc_freq} times" 
-		sum_nk =  sum_nk + doc_freq
-
-    }   
-  end 
-      
-end
-
-
-#personalized classes... I should extend Ferret
-#but well... some day I'll do it properly
-class TermInfo
-    attr_accessor :idf
-    attr_accessor :word_freq
-    attr_accessor :tf
-    
-    def initialize(termstr)
-         @term = termstr
-    end       
-end
-
-class DocumentInfo
-    attr_accessor :words,:id,:total_words
-    def initialize(id,words,ireader)
-        self.id = id
-        self.words = words
-        self.total_words = 0
-        
-        ireader.term_vector(id,:text).terms.each do |t| 
-                self.words[t.text].word_freq = t.positions.size
-                self.total_words = self.total_words + self.words[t.text].word_freq
-                puts "#{t.text} - #{t.positions.size}" 
-        end
-       
-        calculate_tf_df
-               
-    end 
-    
-    def calculate_tf
-             
-        self.words.each  { |k,v| 
-            v.tf = v.word_freq/(self.total_words.to_f)    
-            puts "#{k} - #{v.tf}"
-        }
-                    
-    end    
-end
-  
   
 class MiguelBiAnalyzer <  Ferret::Analysis::Analyzer
   include Ferret::Analysis
@@ -81,7 +14,7 @@ class MiguelBiAnalyzer <  Ferret::Analysis::Analyzer
   def initialize(stop_words = Analysis::FULL_SPANISH_STOP_WORDS, lower = true)
     @lower = lower
     @stop_words = stop_words
-    puts "Hello world"
+    #puts "Hello world"
     
   end
   
@@ -96,6 +29,90 @@ class MiguelBiAnalyzer <  Ferret::Analysis::Analyzer
     
   end
 end
+
+
+#Ferret extension
+
+class Index::IndexReader 
+    @changed = true
+
+         
+   def all_terms
+           all_words = Hash.new
+           ndocs = num_docs.to_f
+           terms(:text).each do |term,doc_freq|
+           #Here we should create all VECTOR META-HASH so we can save Database information about it.. it will a big big hash
+           all_words[term] = TermInfo.new(term)
+           all_words[term].idf = Math::log(ndocs/doc_freq.to_f)
+           all_words[term].word_freq = 0.0
+           all_words[term].tf_idf = 0.0                              
+      	   #puts "#{term} idf-> #{all_words[term].idf}"   
+	       end
+       
+        all_words
+   end 
+             
+end
+
+
+#personalized classes... I should extend Ferret
+#but well... someday I'll do it properly
+class TermInfo
+    attr_accessor :idf
+    attr_accessor :word_freq
+    attr_accessor :tf
+    attr_accessor :tf_idf
+    
+    def initialize(termstr)
+         @term = termstr
+    end       
+end
+
+class DocumentInfo
+    #label = 1 -1 or 0 (for no Label, for transductive set)
+    attr_accessor :terms,:id,:total_words,:label
+    
+    def initialize(id,ireader,label=0)
+        
+        @id = id
+        @terms = ireader.all_terms
+        @total_words = 0
+        @label = label
+        
+        ireader.term_vector(id,:text).terms.each do |t| 
+                @terms[t.text].word_freq = t.positions.size
+                @total_words = @total_words + @terms[t.text].word_freq
+#                puts "#{t.text} - #{t.positions.size}" 
+        end
+       
+        calculate_tf_df
+               
+    end 
+    
+    def calculate_tf_df
+        @terms.each  { |k,v| 
+            v.tf = v.word_freq/(@total_words.to_f)
+            v.tf_idf = v.idf*v.tf
+        #    puts "#{k} - #{v.tf_idf}"
+       }
+                    
+    end    
+    
+    def to_s
+        s = "#{@label} "
+        i = 1;
+        @terms.each do |k,v| 
+            if(v.tf_idf > 0.0) then
+                s = s + "#{i}:#{v.tf_idf} "
+            end
+            i = i + 1    
+        end
+        s = s + "# #{@id}"
+    end
+    
+end
+  
+
 
 
 
@@ -121,25 +138,15 @@ te = index.reader.terms(:text)
 #tv.terms.each {|t| puts "term #{t.text}  with  #{t.positions.size}" }
 #puts Analysis::FULL_SPANISH_STOP_WORDS
 #x = MiguelBiAnalyzer.new
-sum_nk = 0
 
-#write into IndexReader
-all_words = Hash.new
-te.each {|term,doc_freq|
-        #Here we should create all VECTOR META-HASH so we can save Database information about it.. it will a big big hash
-        all_words[term] = TermInfo.new(term)
-        all_words[term].idf = Math::log(index.reader.num_docs.to_f/doc_freq.to_f)
-        all_words[term].word_freq = 0.0
-              
-        #|term, doc_freq| 
-		puts "#{term} idf-> #{all_words[term].idf}" 
-		#sum_nk =  sum_nk + doc_freq
-}
+
+
 
 #now for aeach document we find the words hash but sorted
 
-#doc = DocumentInfo.new(1,all_words.clone,index.reader)
+doc = DocumentInfo.new(1,index.reader)
 
+puts doc
 #make it an inter function
 
 
